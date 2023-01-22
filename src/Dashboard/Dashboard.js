ca988dcc13c18af6bc85225fa1bc7bec6794997d
KushalSandeep <Kushalr@sltc.ac.lk>
2023-01-23 00:48:46 +0530

    dashboard halfway

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,12 +1,31 @@
 import React from "react";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 function Dashboard() {
   return (
     <div>
-      <div>Dashboard</div>
-      <div>Recentlly clicked</div>
-      <div>banner</div>
-      <div>cards</div>
+      <div className="lvl1">
+        <div className="lvl1txt">Dashboard</div>
+        <div className="lvl1cont">
+          <div className="iconset">
+            <NotificationsOutlinedIcon fontSize="inherit" />
+            <SettingsOutlinedIcon fontSize="inherit" />
+            <CalendarMonthOutlinedIcon fontSize="inherit" />
+          </div>
+          <span className="lvl1ssc">
+            <SearchOutlinedIcon />
+            <input placeholder="search" />
+          </span>
+        </div>
+      </div>
+      <div className="lvl2">
+        <div className="lvl3">Recentlly clicked</div>
+        <div className="lvl4">banner</div>
+        <div className="lvl5">cards</div>
+      </div>
     </div>
   );
 }
--- src/Sidebarr.js
+++ src/Sidebarr.js
@@ -1,13 +1,25 @@
 import "./Sidebarr.css";
 import React from "react";
+import card1 from "./images/add1.png";
+import card2 from "./images/add2.png";
+import card3 from "./images/add3.png";
 
 function Sidebarr() {
   return (
     <div>
       <div class="c3">
         <div class="c8">
-          <div class="b1"></div>
-          <div class="b1"></div>
+          <div class="marquee">
+            <div class="b1">
+              <img src={card1} alt="card1" id="card1" />
+            </div>
+            <div class="b1">
+              <img src={card2} alt="card2" id="card2" />
+            </div>
+            <div class="b1">
+              <img src={card3} alt="card3" id="card3" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
